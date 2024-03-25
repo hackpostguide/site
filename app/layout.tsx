9915a0873a74a12c5a042f32a9e4fad443360622
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-25 19:52:28 -0400

    Update metadata and footer in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,56 +8,71 @@ import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "white" },
-		{ media: "(prefers-color-scheme: dark)", color: "black" },
-	],
-	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-touch-icon.png",
-	},
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head />
-			<body
-				className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable
-				)}
-			>
-<Providers themeProps={{ attribute: "class", defaultTheme: "dark", children }}>
-					<div className="relative flex flex-col h-screen">
-						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-							{children}
-						</main>
-						<footer className="w-full flex items-center justify-center py-3">
-							<Link
-								isExternal
-								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-								title="nextui.org homepage"
-							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
-							</Link>
-						</footer>
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark", children }}>
+          <div className="relative flex flex-col h-screen">
+            <Navbar />
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {children}
+            </main>
+            <footer className="w-full flex items-center justify-center py-3">
+              <p className="flex items-center gap-1 text-current">
+                &copy; 2023 - {currentYear} HackPost Guide
+                &bull; 
+				<Link
+					isExternal
+					href="https://www.gnu.org/licenses/gpl-3.0.html#license-text/"
+					title="GNU General Public License v3.0"
+				>
+					<span className="text-default-600">Licensed under GNU General Public License v3.0</span>
+				</Link>
+				&bull; 
+
+				<Link
+                isExternal
+                className="flex items-center gap-1 text-current"
+                href="https://nextui.org"
+                title="nextui.org"
+              >
+                <span className="text-default-600">Powered by NextUI</span>
+                {/* <p className="text-primary">NextUI</p> */}
+              </Link>
+			  </p>
+            </footer>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
