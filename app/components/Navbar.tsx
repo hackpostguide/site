81bffb3c4ebf5ee36bc5ae46074133b2ce089ef2
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-25 19:55:13 -0400

    Add isBordered prop to NextUINavbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -52,7 +52,7 @@ export const Navbar = () => {
 	);
 
 	return (
-		<NextUINavbar maxWidth="xl" position="sticky">
+		<NextUINavbar maxWidth="xl" position="sticky" isBordered>
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-2" href="/">
