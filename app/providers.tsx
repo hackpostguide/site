cb325124599d733eb5c7e891129372d9cb7f7b08
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-25 22:07:59 -0400

    Add UserContext and useUserData hook to Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,6 +6,8 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { UserContext } from "./lib/context";
+import { useUserData } from './lib/hooks';
 
 export interface ProvidersProps {
 	children: React.ReactNode;
@@ -15,9 +17,13 @@ export interface ProvidersProps {
 export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
+  const userData = useUserData();
+
 	return (
-		<NextUIProvider navigate={router.push}>
-			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-		</NextUIProvider>
+		<UserContext.Provider value={userData}>
+			<NextUIProvider navigate={router.push}>
+				<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+			</NextUIProvider>
+		</UserContext.Provider>
 	);
 }
