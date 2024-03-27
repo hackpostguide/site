bf7718dc6a3cb8cc1d27af60228dc4a4757e6854
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-27 11:13:47 -0400

    Refactor UserProfile component to use NextUI Avatar component

--- app/components/UserProfile.tsx
+++ app/components/UserProfile.tsx
@@ -1,18 +1,29 @@
+import { Avatar } from '@nextui-org/react';
 import Img from 'next/image';
 
 export default function UserProfile({ user }: { user: any }) {
-    // console.log('user from UserProfile', user);
   return (
-    <div className="box-center">
-        {/* Add alternate src for photoURL later */}
-      <Img alt="${user.displayName}'s profile picture" src={user.photoURL} width={150} height={150} className="card-img-center" />
-      <p>
-        <i>@{user.username}</i>
-      </p>
-      <h1>{user.displayName}</h1>
-      <h2>About Me:</h2>
-      <p>{user.bio}</p>
-      {/* <p>User Profile</p> */}
+    <div>
+      <div className= "flex justify-center items-center">
+        <div
+          className="p-8 rounded-lg shadow-lg flex flex-col items-center w-1/3"
+        >
+          <Avatar
+            showFallback // Show fallback avatar if src is not provided
+            color="primary"
+            name={user?.displayName ?? ''}
+            // size=""
+            src={user?.photoURL ?? ''}
+            className="w-40 h-40 mb-4"
+          />
+          <p className="mb-2 text-default-600 text-xl">
+            <i>@{user.username}</i>
+          </p>
+          <h1 className="text-default-foreground mb-2 text-2xl">{user.displayName}</h1>
+          <h2 className="text-gray-400 mb-4">About Me:</h2>
+          <p className="text-gray-300">{user.bio}</p>
+        </div>
+      </div>
     </div>
   );
 }
