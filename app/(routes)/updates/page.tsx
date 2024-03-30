2f7b69945e4a04344ca26a7edfcd97aea839964f
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-29 22:00:15 -0400

    Add content to Updates page

--- app/(routes)/updates/page.tsx
+++ app/(routes)/updates/page.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Updates = () => {
   return (
-    <div>Updates</div>
+    <div>
+      <h1>
+        Welcome to the Updates page!
+      </h1>
+      <p>
+        This page page is where you can find the latest updates about the Hackpost Guide app, and trust me, there are a lot of updates coming soon!
+        This project was made in early 2024 and is still in development, so there are a lot of features that are still being worked on.
+        If you would like to contribute to the project, feel free to check out the GitHub repository and submit a pull request!
+      </p>
+    </div>
   )
 }
 
