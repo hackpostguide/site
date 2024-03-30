7bdb852f615538ac606eaee499b592b48968aa44
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-30 09:53:57 -0400

    Add dashboard page and related components

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,9 +1,101 @@
+// app/admin/page.tsx
+'use client';
 import React from 'react'
+// import styles from '../styles/Admin.module.css'
+import AuthCheck from '@/app/components/(dashboard)/AuthCheck'
+import PostFeed from '@/app/components/(postComponents)/PostCard'
+import { UserContext } from '@/app/lib/context'
+import { auth } from '@/app/lib/firebase'
+import { serverTimestamp, query, collection, orderBy, getFirestore, setDoc, doc } from 'firebase/firestore'
+import { useContext, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import kebabCase from 'lodash.kebabcase'
+import toast from 'react-hot-toast'
 
-const Dashboard = () => {
+export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <main>
+      {/* <AuthCheck>
+        @ts-expect-error Server Component
+        <PostList />
+        @ts-expect-error Server Component
+        <CreateNewPost />
+      </AuthCheck> */}
+      <AuthCheck>
+        <PostList />
+        <CreateNewPost />
+      </AuthCheck>
+    </main>
   )
 }
 
-export default Dashboard
+const PostList = (): JSX.Element => {
+  const uid: any = auth?.currentUser?.uid
+  const ref = collection(getFirestore(), 'users', uid, 'posts')
+  const postQuery = query(ref, orderBy('createdAt'))
+  const [querySnapshot] = useCollection(postQuery)
+  const posts: any = querySnapshot?.docs.map((doc: any) => doc.data())
+
+  return (
+    <>
+      <h1>Manage your Posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  )
+}
+
+const CreateNewPost = (): JSX.Element => {
+  const router = useRouter()
+  const { username } = useContext(UserContext)
+  const [title, setTitle] = useState('')
+
+  // Ensure slug is URL safe
+  const slug = encodeURI(kebabCase(title))
+
+  // Validate length
+  const isValid = title.length > 3 && title.length < 100
+
+  // Create a new post in firestore
+  const createPost = async (e: any): Promise<void> => {
+    e.preventDefault()
+    const uid: any = auth?.currentUser?.uid
+    const ref = doc(getFirestore(), 'users', uid, 'posts', slug)
+
+    // Tip: give all fields a default value here
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: '# hello world!',
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    }
+
+    await setDoc(ref, data)
+    toast.success('Post created!')
+
+    // Imperative navigation after doc is set
+    router.push(`/admin/${slug}`)
+  }
+
+  return (
+    <form onSubmit={createPost}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My Awesome Article!"
+        // className={styles.input}
+      />
+      <p>
+        <strong>Slug:</strong> {slug}
+      </p>
+      <button type="submit" disabled={!isValid} className="btn-green">
+        Create New Post
+      </button>
+    </form>
+  )
+}
---
+++ app/components/(dashboard)/AuthCheck.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+import { useContext } from 'react';
+import { UserContext } from '@/app/lib/context';
+
+// Component's children only shown to logged-in users
+export default function AuthCheck(props: any): JSX.Element {
+    const { username } = useContext(UserContext);
+    return username ? props.children : props.fallback || <Link href="/enter">You must be signed in</Link>;
+}
