7e500fbd8bc1f187bebefc12e3a1ec15727a84de
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-31 19:50:12 -0400

    Add content and styling to About Page

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -4,6 +4,13 @@ const AboutPage = () => {
   return (
     <div>
       <h1>About Page</h1>
+      <p>
+      This project was developed to consolidate resources for hackathons and foster a community passionate about creating various things - apps, games, websites, and more. The resource hub is designed to resemble scratch.mit.edu and dev.to. We look forward to seeing your contributions to this expanding community!
+      <br />
+      This project was made using Next JS, Firebase, and Next UI. It is hosted on Vercel. This project is also still VERY much work in progress (check out updates for more information) - if you would like to contribute to this project, feel free to check out our Github repository! Likewise, make sure to also join our discord server, and also sign up to our mailing list to receive updates and news. 
+      <br />
+      For now, feel free to make an account and post what you’re working on! Make sure to follow the community guidelines and
+      </p>
     </div>
   )
 }
