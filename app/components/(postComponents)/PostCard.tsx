c4179d472b143a10dffb41a1c34216b7559748b1
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-31 12:09:16 -0400

    Refactored PostCard UI

--- app/components/(postComponents)/PostCard.tsx
+++ app/components/(postComponents)/PostCard.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Card, CardHeader, CardBody, CardFooter, Divider } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Button } from "@nextui-org/react";
 
 export default function PostCard({ posts, admin = false }: { posts: any[], admin?: boolean }) {
   return posts && posts.length ? <>{posts.map((post: any, i: number) => <PostItem post={post} key={i} admin={admin} />)}</> : <></>;
@@ -12,34 +12,32 @@ function PostItem({ post, admin }: { post: any, admin: boolean }) {
 
   return (
     <Card className="max-w-[400px]" isPressable>
-      <CardHeader className="flex gap-3">
+      <CardHeader className="flex gap-3 justify-between">
         <Link passHref href={`/users/${post.username}`}>
           <strong>By @{post.username}</strong>
         </Link>
+        {/* If admin view, show extra controls for user */}
+        {admin && (
+          <>
+            <Button as={Link} href={`/dashboard/${post.slug}`} size="sm">
+              Edit
+            </Button>
+          </>
+        )}
       </CardHeader>
       <Divider />
       <Link passHref href={`/users/${post.username}/${post.slug}`}>
         <CardBody>
-          <h2 className='text-xl'>{post.title}</h2>
+          <h2 className='text-xl font-bold'>{post.title}</h2>
           <p className="text-default-600">{post.content}</p>
+          {post.published ? <p className="text-success">Published</p> : <p className="text-danger">Unpublished</p>}
         </CardBody>
-        <CardFooter>
+        <CardFooter className='flex justify-between'>
           <span>{minutesToRead} min read</span>
           <span className="">💗 {post.heartCount || 0} Hearts</span>
           <span>Views: {post.views}</span>
         </CardFooter>
       </Link>
-      {/* If admin view, show extra controls for user */}
-      {admin && (
-        <>
-          <Link passHref href={`/dashboard/${post.slug}`}>
-            <h3>
-              <button className="btn-blue">Edit</button>
-            </h3>
-          </Link>
-          {post.published ? <p className="text-success">Live</p> : <p className="text-danger">Unpublished</p>}
-        </>
-      )}
     </Card>
   );
 }
