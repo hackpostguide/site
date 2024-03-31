ae43766238d27b9656f8b56c6d00c918c6afdfd8
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-31 19:25:29 -0400

    Refactor PostContent component to use NextUI Card component

--- app/components/(postComponents)/PostContent.tsx
+++ app/components/(postComponents)/PostContent.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
 import { useUpdateViews } from '@/app/components/(postComponents)/hooks/useUpdateViews';
 import { MarkdownComponents } from './MarkdownComponents';
+import { Card, CardBody, CardHeader, Link } from '@nextui-org/react';
 
 export default function PostContent({ post, path }: { post: any, path: string }) {
   const createdAt = typeof post?.createdAt === 'number' ? new Date(post.createdAt) : post.createdAt.toDate();
@@ -9,15 +9,20 @@ export default function PostContent({ post, path }: { post: any, path: string })
   // useUpdateViews(post.slug, path); 
 
   return (
-    <div className="card">
-      <h1>{post?.title}</h1>
-      <span className="text-sm">
-        Written by{' '}
-        <Link href={`/users/${post.username}/`} className="text-info">
-          @{post.username}
-        </Link>{' '}
-        on {createdAt.toISOString()}
-      </span>
+    <div className="">
+      <Card className='p-5'>
+        <CardBody>
+          <h1 className='text-center mb-6'>{post?.title}</h1>
+          <p className='text-center'>
+            Written by{' '}
+            <Link href={`/users/${post.username}/`} underline="hover">
+              @{post.username}
+            </Link>{' '}
+            on {createdAt.toISOString()}
+          </p>
+        </CardBody>
+        
+      </Card>
       <ReactMarkdown components={MarkdownComponents}>{post?.content}</ReactMarkdown>
       {/* <p>Views: {post.views}</p> */}
     </div>
