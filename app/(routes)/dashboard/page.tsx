1ac2aabf04fe69feb9952e033854f473ad015dd9
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-31 11:19:18 -0400

    Added comments

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -10,18 +10,12 @@ import { serverTimestamp, query, collection, orderBy, getFirestore, setDoc, doc 
 import { useContext, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useCollection } from 'react-firebase-hooks/firestore'
-import kebabCase from 'lodash.kebabcase'
+import kebabCase from 'lodash.kebabcase' //implement kebabCase for slug later?
 import toast from 'react-hot-toast'
 
 export default function Dashboard() {
   return (
     <main>
-      {/* <AuthCheck>
-        @ts-expect-error Server Component
-        <PostList />
-        @ts-expect-error Server Component
-        <CreateNewPost />
-      </AuthCheck> */}
       <AuthCheck>
         <PostList />
         <CreateNewPost />
@@ -50,26 +44,18 @@ const CreateNewPost = (): JSX.Element => {
   const { username } = useContext(UserContext);
   const [title, setTitle] = useState('');
 
-  //use server timestamp to generate a unique slug in the future
-  // // Get the server timestamp
-  // const serverTime = serverTimestamp();
-  // console.log('serverTime', serverTime.toString());
+  //TODO: use server timestamp instead to generate a unique slug in the future
 
-  // // Use the number of seconds since 1/1/2000 as the slug
-  // const slug = serverTime.toString();
-
-
+  //slug generation: 
   // Get the current timestamp in milliseconds
   const currentTimestamp = Date.now();
-
   // Convert milliseconds to seconds
   const secondsSinceEpoch = Math.floor(currentTimestamp / 1000);
-
   // Subtract the number of seconds between 1/1/1970 and 1/1/2000
   const secondsSince2000 = secondsSinceEpoch - 946684800;
-
   // Use the number of seconds since 1/1/2000 as the slug
   const slug = "post-" + secondsSince2000.toString();
+
 
   // Validate length
   const isValid = title.length > 3 && title.length < 100;
