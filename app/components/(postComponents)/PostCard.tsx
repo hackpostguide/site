af4e4a409c26a186dcb7a590acf98feaba7a366e
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-03-31 18:59:22 -0400

    Refactor post content rendering to limit text length

--- app/components/(postComponents)/PostCard.tsx
+++ app/components/(postComponents)/PostCard.tsx
@@ -1,6 +1,12 @@
 import Link from 'next/link';
 import { Card, CardHeader, CardBody, CardFooter, Divider, Button } from "@nextui-org/react";
 
+// renders text, only first 200 characters of a post
+const LimitedTextRenderer: React.FC<{ value: string }> = ({ value }) => {
+  return value.length > 200 ? `${value.substring(0, 200)}...` : value;
+};
+
+
 export default function PostCard({ posts, admin = false }: { posts: any[], admin?: boolean }) {
   return posts && posts.length ? <>{posts.map((post: any, i: number) => <PostItem post={post} key={i} admin={admin} />)}</> : <></>;
 }
@@ -29,7 +35,7 @@ function PostItem({ post, admin }: { post: any, admin: boolean }) {
       <Link passHref href={`/users/${post.username}/${post.slug}`}>
         <CardBody>
           <h2 className='text-xl font-bold'>{post.title}</h2>
-          <p className="text-default-600">{post.content}</p>
+          <p className="text-default-600">{post.content.length > 200 ? `${post.content.substring(0, 200)}...` : post.content}</p>
           {admin && (post.published ? <p className="text-success">Published</p> : <p className="text-danger">Unpublished</p>)}
         </CardBody>
         <CardFooter className='flex justify-between'>
