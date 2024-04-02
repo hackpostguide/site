8e2d6f99abb3c2a4428c5e0647fb4a238e812b9d
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-01 23:32:16 -0400

    Update AboutPage content

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
 const AboutPage = () => {
-  return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-      This project was developed to consolidate resources for hackathons and foster a community passionate about creating various things - apps, games, websites, and more. The resource hub is designed to resemble scratch.mit.edu and dev.to. We look forward to seeing your contributions to this expanding community!
-      <br />
-      This project was made using Next JS, Firebase, and Next UI. It is hosted on Vercel. This project is also still VERY much work in progress (check out updates for more information) - if you would like to contribute to this project, feel free to check out our Github repository! Likewise, make sure to also join our discord server, and also sign up to our mailing list to receive updates and news. 
-      <br />
-      For now, feel free to make an account and post what you’re working on! Make sure to follow the community guidelines and
-      </p>
-    </div>
-  )
+    return (
+      <div>
+        <h1>About this project</h1>
+        <p>
+        🚀 Hackpost Guide was developed to consolidate resources for hackathons and foster a community passionate about creating various things - apps, games, websites, and more. The resource hub is designed to resemble scratch.mit.edu and dev.to. We look forward to seeing your contributions to this expanding community! 🌐
+        <br /> <br />
+        This project was made using Next JS, Firebase, and Next UI. It is hosted on Vercel. This project is also still VERY much a work in progress (check out updates for more information) - if you would like to contribute to this project, feel free to check out our Github repository! Likewise, make sure to also join our Discord server 🎮, and also sign up to our mailing list 📧 to receive updates and news.
+        <br /> <br />
+        For now, feel free to make an account and post what you’re working on! Make sure to follow the community guidelines and maintain a positive and respectful environment. Let&apos;s build something amazing together! ✨🔨
+        <br /> <br />
+        Remember, whether you&apos;re a seasoned developer or just starting out, your insights and projects are valuable to the Hackpost Guide community. Let&apos;s innovate, collaborate, and create the future of tech, together! 🌟
+        </p>
+      </div>
+    )
 }
 
 export default AboutPage
