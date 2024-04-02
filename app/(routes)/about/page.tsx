de79ecf569b625cecfb53f99fce24a312242108d
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-02 17:59:39 -0400

    Update links in about and updates pages

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -9,9 +9,9 @@ const AboutPage = () => {
         <p>
         🚀 Hackpost Guide was developed to consolidate resources for hackathons and foster a community passionate about creating various things - apps, games, websites, and more. The resource hub is designed to resemble scratch.mit.edu and dev.to. We look forward to seeing your contributions to this expanding community! 🌐
         <br /> <br />
-        This project was made using Next JS, Firebase, and Next UI. It is hosted on Vercel. This project is also still VERY much a work in progress (check out updates for more information) - if you would like to contribute to this project, feel free to check out our Github repository(https://github.com/hackpostguide/site)! Likewise, make sure to also join our Discord server 🎮.
+        This project was made using Next JS, Firebase, and Next UI. It is hosted on Vercel. This project is also still VERY much a work in progress (check out updates for more information) - if you would like to contribute to this project, feel free to check out our Github repository (link in the navbar)! Likewise, make sure to also join our Discord server (invite link also in the navbar)🎮.
         <br /> <br />
-        For now, feel free to make an account and post what you’re working on! Make sure to follow the community guidelines and maintain a positive and respectful environment. Let&apos;s build something amazing together! ✨🔨
+        For now, feel free to make an account and post what you’re working on! Make sure to follow the community guidelines and maintain a positive and respectful environment. ✨🔨
         <br /> <br />
         Remember, whether you&apos;re a seasoned developer or just starting out, your insights and projects are valuable to the Hackpost Guide community. Let&apos;s innovate, collaborate, and create the future of tech, together! 🌟
         </p>
