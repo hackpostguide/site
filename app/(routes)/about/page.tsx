781f728a97f50ddc020625f06458b6134593f1bc
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-01 23:34:21 -0400

    Update GitHub repository links in about and updates pages

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
         <p>
         🚀 Hackpost Guide was developed to consolidate resources for hackathons and foster a community passionate about creating various things - apps, games, websites, and more. The resource hub is designed to resemble scratch.mit.edu and dev.to. We look forward to seeing your contributions to this expanding community! 🌐
         <br /> <br />
-        This project was made using Next JS, Firebase, and Next UI. It is hosted on Vercel. This project is also still VERY much a work in progress (check out updates for more information) - if you would like to contribute to this project, feel free to check out our Github repository! Likewise, make sure to also join our Discord server 🎮, and also sign up to our mailing list 📧 to receive updates and news.
+        This project was made using Next JS, Firebase, and Next UI. It is hosted on Vercel. This project is also still VERY much a work in progress (check out updates for more information) - if you would like to contribute to this project, feel free to check out our Github repository(https://github.com/hackpostguide/site)! Likewise, make sure to also join our Discord server 🎮.
         <br /> <br />
         For now, feel free to make an account and post what you’re working on! Make sure to follow the community guidelines and maintain a positive and respectful environment. Let&apos;s build something amazing together! ✨🔨
         <br /> <br />
--- app/(routes)/updates/page.tsx
+++ app/(routes)/updates/page.tsx
@@ -10,7 +10,7 @@ const Updates = () => {
       <p>
         This page page is where you can find the latest updates about the Hackpost Guide app, and trust me, there are a lot of updates coming soon!
         This project was made in early 2024 and is still in development, so there are a lot of features that are still being worked on.
-        If you would like to contribute to the project, feel free to check out the GitHub repository and submit a pull request!
+        If you would like to contribute to the project, feel free to check out the GitHub repository (https://github.com/hackpostguide/site) and submit a pull request!
       </p>
     </div>
   )
