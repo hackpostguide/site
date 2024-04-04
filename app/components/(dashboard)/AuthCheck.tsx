687bc52dc9d0d2847c99acf10cea76c9b96811c4
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-03 21:50:34 -0400

    Refactor AuthCheck component to include a paragraph element for the "You must be signed in" message

--- app/components/(dashboard)/AuthCheck.tsx
+++ app/components/(dashboard)/AuthCheck.tsx
@@ -5,5 +5,7 @@ import { UserContext } from '@/app/lib/context';
 // Component's children only shown to logged-in users
 export default function AuthCheck(props: any): JSX.Element {
     const { username } = useContext(UserContext);
-    return username ? props.children : props.fallback || <Link href="/enter">You must be signed in</Link>;
+    return username ? props.children : props.fallback || <Link href="/enter">
+        <p>You must be signed in</p>
+    </Link>;
 }
