a6c7f336ab975b95267e250a49362bd242306524
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-08 23:06:55 -0400

    Update layout.tsx and providers.tsx to use https://www.npmjs.com/package/next-nprogress-bar instead

--- app/providers.tsx
+++ app/providers.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { UserContext } from "./lib/context";
 import { useUserData } from './lib/hooks';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 export interface ProvidersProps {
 	children: React.ReactNode;
@@ -21,6 +22,12 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 
 	return (
 		<UserContext.Provider value={userData}>
+			<ProgressBar
+				height="4px"
+				color="#FF0080"
+				options={{ showSpinner: false }}
+				shallowRouting
+			/>
 			<NextUIProvider navigate={router.push}>
 				<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
 			</NextUIProvider>
