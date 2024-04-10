27b9c1cfbbce2a68380842a0ff95c7271559df87
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-09 22:10:10 -0400

    Refactor PostContent component to improve readability and add formatted date

--- app/components/(postComponents)/PostContent.tsx
+++ app/components/(postComponents)/PostContent.tsx
@@ -5,23 +5,28 @@ import { Card, CardBody, CardHeader, Link } from '@nextui-org/react';
 
 export default function PostContent({ post, path }: { post: any, path: string }) {
   const createdAt = typeof post?.createdAt === 'number' ? new Date(post.createdAt) : post.createdAt.toDate();
+  // useUpdateViews(post.slug, path);
 
-  // useUpdateViews(post.slug, path); 
+  const formattedDate = createdAt.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  });
 
   return (
     <div className="">
       <Card className='p-5'>
         <CardBody>
           <h1 className='text-center mb-6'>{post?.title}</h1>
-          <p className='text-center'>
-            Written by{' '}
-            <Link href={`/users/${post.username}/`} underline="hover">
-              @{post.username}
-            </Link>{' '}
-            on {createdAt.toISOString()}
-          </p>
+          <div className='flex justify-center items-center mb-6'>
+            <p>Written by</p>
+            <Link href={`/users/${post.username}/`} underline="hover" className="mx-2">
+              <p>@{post.username}</p>
+            </Link>
+            <p>on</p>
+            <p className="ml-2">{formattedDate}</p>
+          </div>
         </CardBody>
-        
       </Card>
       <ReactMarkdown components={MarkdownComponents} className="my-5">{post?.content}</ReactMarkdown>
       {/* <p>Views: {post.views}</p> */}
