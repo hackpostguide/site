e87c8b6f88a3d6f4fbd0901305b3bb32ebe21499
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-09 23:14:56 -0400

    Refactor PostCard component to improve readability and add admin controls

--- app/components/(postComponents)/PostCard.tsx
+++ app/components/(postComponents)/PostCard.tsx
@@ -12,34 +12,38 @@ function PostItem({ post, admin }: { post: any, admin: boolean }) {
 
 
   return (
-    <Card className="max-w-[400px] p-3 relative hover:-translate-y-1 after:content-[''] after:absolute after:inset-0 after:bg-background/40 after:z-[-1] after:transition after:!duration-500 hover:after:scale-150 hover:after:opacity-0" isPressable>
-      <CardHeader className="flex gap-3 justify-between">
-        <Link color="warning" passHref href={`/users/${post.username}`}>
-          <strong>By @{post.username}</strong>
+    <Card className="max-w-[400px] p-3 relative hover:-translate-y-1 after:content-[''] after:absolute after:inset-0 after:bg-background/40 after:z-[-1] after:transition after:!duration-500 hover:after:scale-150 hover:after:opacity-0 flex flex-col justify-between" isPressable>
+      <div>
+        <CardHeader className="text-secondary flex gap-3 justify-between">
+          <Link passHref href={`/users/${post.username}`}>
+            <strong>By @{post.username}</strong>
+          </Link>
+          {/* If admin view, show extra controls for user */}
+          {admin && (
+            <>
+              <Button as={Link} href={`/dashboard/${post.slug}`} size="sm">
+                Edit
+              </Button>
+            </>
+          )}
+        </CardHeader> 
+        <Divider />
+        <Link passHref href={`/users/${post.username}/${post.slug}`}>
+          <CardBody className="">
+            <h2 className='text-xl font-bold text-primary'>{post.title}</h2>
+            <p className="text-default-600">{post.content.length > 200 ? `${post.content.substring(0, 200)}...` : post.content}</p>
+            {admin && (post.published ? <p className="text-success">Published</p> : <p className="text-danger">Unpublished</p>)}
+          </CardBody>
         </Link>
-        {/* If admin view, show extra controls for user */}
-        {admin && (
-          <>
-            <Button as={Link} href={`/dashboard/${post.slug}`} size="sm">
-              Edit
-            </Button>
-          </>
-        )}
-      </CardHeader> 
-      <Divider />
-      <Link passHref href={`/users/${post.username}/${post.slug}`}>
-        <CardBody className="">
-          <h2 className='text-xl font-bold'>{post.title}</h2>
-          <p className="text-default-600">{post.content.length > 200 ? `${post.content.substring(0, 200)}...` : post.content}</p>
-          {admin && (post.published ? <p className="text-success">Published</p> : <p className="text-danger">Unpublished</p>)}
-        </CardBody>
-        {/* <CardFooter className='flex justify-start gap-12'> */}
-        <CardFooter className='flex justify-start gap-40'>
-          <span>{minutesToRead} min read</span>
-          <span className="">💗 {post.heartCount || 0} Hearts</span>
-          {/* <span>Views: {post.views}</span> */}
-        </CardFooter>
-      </Link>
+      </div>
+      
+      {/* <CardFooter className='flex justify-start gap-12'> */}
+      <CardFooter className='flex justify-start gap-40 text-warning font-bold'>
+        <span>{minutesToRead} min read</span>
+        <span className="">💖 {post.heartCount || 0} Hearts</span>
+        {/* <span>Views: {post.views}</span> */}
+      </CardFooter>
+      
     </Card>
   );
 }
