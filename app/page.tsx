c4d5a02fb1b4ba75d468b845d8d0186268bf89d7
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-12 18:31:23 -0400

    Update hero text and button styles in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 		{/* Align the hero page text to the left later */}
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Jump right in into the &nbsp;</h1>
+				<h1 className={title()}>Jump right in into the&nbsp;</h1>
 				<h1 className={title({color: "blue" })}>open source&nbsp;</h1>
 				<h1 className={title()}>resource hub for&nbsp;</h1>
 				<h1 className={title({ color: "violet" })}>hackathon participants.&nbsp;</h1>
