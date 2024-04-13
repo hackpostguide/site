5d456761f5fd12b03d439cbb6293c68c211ba909
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-13 10:35:28 -0400

    Added check to see if a post is published

--- app/(routes)/users/[username]/[slug]/page.tsx
+++ app/(routes)/users/[username]/[slug]/page.tsx
@@ -1,5 +1,3 @@
-
-// app/[username]/[slug]/page.tsx
 import styles from '@/app/styles/Post.module.css';
 import PostContent from '@/app/components/(postComponents)/PostContent';
 import { firestore, getUserWithUsername, postToJSON } from '@/app/lib/firebase';
@@ -8,7 +6,6 @@ import { useDocumentData } from 'react-firebase-hooks/firestore';
 import { UserContext } from '@/app/lib/context';
 import { useContext } from 'react';
 import Metatags from '@/app/components/Metatags';
-// import AuthCheck from '@/app/components/AuthCheck';
 import HeartButton from '@/app/components/(postComponents)/HeartButton';
 import Link from 'next/link';
 import HeartCard from '@/app/components/(postComponents)/HeartCard';
@@ -17,56 +14,31 @@ async function getPost(username: string, slug: string) {
   const userDoc = await getUserWithUsername(username);
   let post;
   let path;
-
   if (userDoc) {
     const postRef = doc(getFirestore(), userDoc.ref.path, 'posts', slug);
     post = postToJSON(await getDoc(postRef));
     path = postRef.path;
-
-    // Increment the "views" count
-    // await updateDoc(postRef, {
-    //   views: increment(1),
-    // });
   }
-
-  return {
-    props: { post, path },
-    revalidate: 100,
-  };
+  return { props: { post, path }, revalidate: 100 };
 }
 
 export default async function Post({ params }: { params: { username: string; slug: string } }) {
   const { props } = await getPost(params.username, params.slug);
   const { post, path } = props;
-  // const postRef = path ? doc(getFirestore(), path) : null;
-  // console.log('post', post);
-  // console.log('path', path);
-  // console.log('postRef', postRef);
-  // const [realtimePost] = useDocumentData(postRef);
-
-  // const postData = post;
-
-  // console.log('post title:', post?.title);
-
-  // const { user: currentUser } = useContext(UserContext);
 
   return (
     <main className={`${styles.container} mb-[100px]`}>
       <Metatags title={post?.title} description={post?.title} image='' />
-      {/* Metatags do not work for some reason */}
-
-      {/* DEBUG */}
-      {/* <p>post: {post}</p> */}
-      {/* <p>path: {path}</p> */}
-      {/* <p>postRef: {String(postRef)}</p> */}
-      {/* <p>postRef.path: {postRef?.path}</p> */}
-
       <section>
-        <PostContent post={post} path={path || ''} />
+        {post?.published ? (
+          <div>
+            <PostContent post={post} path={path || ''} />
+            <HeartCard post={post} path={path} />
+          </div>
+        ) : (
+          <p>This post is currently private.</p>
+        )}
       </section>
-
-      <HeartCard post={post} path={path} />
-
     </main>
   );
 }
