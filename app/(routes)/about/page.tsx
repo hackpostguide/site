cbe123db6f06ed9573cf47b48c1207d0cbeecd1d
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-13 09:29:23 -0400

    Update AboutPage content in page.tsx

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
-
-// TODO: Add a link to the GitHub repository, and a link to the Discord server. 
+import { siteConfig } from "@/app/config/site";
+import { Link } from '@nextui-org/react';
 
 const AboutPage = () => {
-    return (
-      <div>
-        <h1>About Hackpost Guide 🚀</h1>
-        <p>
-        Hackpost Guide is an open-source web app developed to consolidate resources for hackathons and foster a community passionate about creating apps, games, websites, and projects. It is designed after scratch.mit.edu and dev.to.
-        <br /> <br />
-        For now, feel free to make an account and check out some posts/guides that others have created! Make sure to follow the community guidelines and maintain a positive and respectful environment.
-        <br /> <br />
-        This project is still VERY much a work in progress and in its early stages. If you find a bug, or if you would like to contribute, check out our Github repository! Make sure to also join our discord server: ______.
-        </p>
-      </div>
-    )
+  return (
+    <div>
+      <h1>About Hackpost Guide 🚀</h1>
+      <p>
+        Hackpost Guide is an open-source web app developed to consolidate resources for hackathons and foster a community passionate about creating apps, games, websites, and projects. It is designed after scratch.mit.edu and dev.to. 
+        <br /><br />
+        Hackpost Guide promotes creativity and innovation, collaboration, and equity in computing.
+        <br /><br />
+        For now, feel free to make an account and check out some posts/guides that others have created! Make sure to follow the community guidelines and maintain a positive and respectful environment. 
+        <br /><br />
+        This project is still VERY much a work in progress and in its early stages. If you find a bug, or if you would like to contribute, check out our <Link isExternal href={siteConfig.links.github} aria-label="Discord"> <p>Github</p> </Link>. Make sure to also join our <Link isExternal href={siteConfig.links.discord} aria-label="Discord"> <p>Discord Server</p> </Link>.
+      </p>
+    </div>
+  )
 }
 
 export default AboutPage
