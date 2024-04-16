458fdfd5e533cc343dd26854ede22f6b2c77de27
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-16 18:15:32 -0400

    Refactor ImageUploader component

--- app/components/(dashboard)/ImageUploader.tsx
+++ app/components/(dashboard)/ImageUploader.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { auth, storage, STATE_CHANGED } from '@/app/lib/firebase';
 import Loader from '@/app/components/Loader';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
-import { Button } from '@nextui-org/react';
+import { Button, Tooltip } from '@nextui-org/react';
 import { Icon } from '@iconify/react';
 
 // Uploads images to Firebase Storage
@@ -10,34 +10,34 @@ export default function ImageUploader(): JSX.Element {
     const [uploading, setUploading] = useState(false);
     const [progress, setProgress] = useState(0);
     const [downloadURL, setDownloadURL] = useState(null);
-    const [fileInputRef, setFileInputRef] = useState<HTMLInputElement | null>(null);
-
+    const fileInputRef = useRef<HTMLInputElement>(null);
+  
     // Creates a Firebase Upload Task
     const uploadFile = async () => {
-        if (fileInputRef) {
-        const file = fileInputRef.files?.[0] as Blob;
+      if (fileInputRef.current) {
+        const file = fileInputRef.current.files?.[0] as Blob;
         const extension = file?.type.split('/')[1];
         const uid: any = auth?.currentUser?.uid;
         const fileRef = ref(storage, `uploads/${uid}/${Date.now()}.${extension}`);
         setUploading(true);
-
+  
         // Starts the upload
         const task = uploadBytesResumable(fileRef, file);
-
+  
         // Listen to updates to upload task
         task.on(STATE_CHANGED, (snapshot) => {
-            const pct: any = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
-            setProgress(pct);
+          const pct: any = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
+          setProgress(pct);
         });
-
+  
         // Get downloadURL AFTER task resolves (Note: this is not a native Promise)
         task
-            .then(() => getDownloadURL(fileRef))
-            .then((url: any) => {
+          .then(() => getDownloadURL(fileRef))
+          .then((url: any) => {
             setDownloadURL(url);
             setUploading(false);
-            });
-        }
+          });
+      }
     };
 
     return (
@@ -45,19 +45,26 @@ export default function ImageUploader(): JSX.Element {
         <Loader show={uploading} />
         {uploading && <h3>{progress}%</h3>}
         {!uploading && (
-            <Button 
-                color="secondary" 
-                endContent={<Icon icon="tabler:camera-plus" width="24" height="24" />}
-                onClick={() => fileInputRef?.click()}
+            <Tooltip 
+                offset={15}
+                placement= "top-end"
+                content="I am a tooltip"
             >
-            Upload Image
-            <input
-                type="file"
-                ref={setFileInputRef}
-                accept="image/x-png,image/gif,image/jpeg, image/jpg"
-                style={{ display: 'none' }}
-            />
-            </Button>
+                <Button 
+                    onClick={uploadFile}
+                    color="secondary" 
+                    endContent={<Icon icon="tabler:camera-plus" width="24" height="24" />}
+                    // onClick={() => fileInputRef?.click()}
+                >
+                Upload Image
+                <input
+                    type="file"
+                    ref={fileInputRef}
+                    accept="image/x-png,image/gif,image/jpeg, image/jpg"
+                    style={{ display: 'none' }}
+                />
+                </Button>
+            </Tooltip>
         )}
         {downloadURL && <code className="upload-snippet">{`![alt](${downloadURL})`}</code>}
         </div>
