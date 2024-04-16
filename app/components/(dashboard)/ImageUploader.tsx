e8a38f163e6e293d2cfe4235d9419de3159a9ede
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-16 18:27:38 -0400

    Refactor ImageUploader component to fix file input reference issue and improve code readability

--- app/components/(dashboard)/ImageUploader.tsx
+++ app/components/(dashboard)/ImageUploader.tsx
@@ -14,30 +14,36 @@ export default function ImageUploader(): JSX.Element {
   
     // Creates a Firebase Upload Task
     const uploadFile = async () => {
-      if (fileInputRef.current) {
+        if (fileInputRef.current) {
         const file = fileInputRef.current.files?.[0] as Blob;
         const extension = file?.type.split('/')[1];
         const uid: any = auth?.currentUser?.uid;
         const fileRef = ref(storage, `uploads/${uid}/${Date.now()}.${extension}`);
         setUploading(true);
-  
+
         // Starts the upload
         const task = uploadBytesResumable(fileRef, file);
-  
+
         // Listen to updates to upload task
         task.on(STATE_CHANGED, (snapshot) => {
-          const pct: any = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
-          setProgress(pct);
+            const pct: any = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
+            setProgress(pct);
         });
-  
+
         // Get downloadURL AFTER task resolves (Note: this is not a native Promise)
         task
-          .then(() => getDownloadURL(fileRef))
-          .then((url: any) => {
+            .then(() => getDownloadURL(fileRef))
+            .then((url: any) => {
             setDownloadURL(url);
             setUploading(false);
-          });
-      }
+            });
+        }
+    };
+
+    const handleFileChange = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
     };
 
     return (
@@ -50,19 +56,15 @@ export default function ImageUploader(): JSX.Element {
                 placement= "top-end"
                 content="I am a tooltip"
             >
-                <Button 
-                    onClick={uploadFile}
-                    color="secondary" 
-                    endContent={<Icon icon="tabler:camera-plus" width="24" height="24" />}
-                    // onClick={() => fileInputRef?.click()}
-                >
-                Upload Image
-                <input
+                <Button color="secondary" onClick={handleFileChange} endContent={<Icon icon="tabler:camera-plus" width="24" height="24" />}>
+                    Upload Image
+                    <input
                     type="file"
                     ref={fileInputRef}
-                    accept="image/x-png,image/gif,image/jpeg, image/jpg"
+                    accept="image/x-png,image/gif,image/jpeg"
                     style={{ display: 'none' }}
-                />
+                    onChange={uploadFile}
+                    />
                 </Button>
             </Tooltip>
         )}
