d5391d8bec019ae89c20ed93eead05c6c5d1f114
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-25 11:10:44 -0400

    Added button style in error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,6 @@
 'use client' 
  
+import { Button } from '@nextui-org/button'
 import { useEffect } from 'react'
  
 export default function Error({
@@ -17,14 +18,14 @@ export default function Error({
   return (
     <div>
       <h2>Something went wrong!</h2>
-      <button
+      <Button size='md'
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
         Try again
-      </button>
+      </Button>
     </div>
   )
 }
