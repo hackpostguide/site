cf7e7e7a23b35a67c0ea9fa090fbd6115ab091b0
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-25 10:00:13 -0400

    Update terms and conditions page with Hackpost Guide's Terms of Service

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,9 @@
-import type { MDXComponents } from 'mdx/types'
- 
+import type { MDXComponents } from 'mdx/types';
+import { MarkdownComponents } from '@/app/components/(postComponents)/MarkdownComponents';
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-  }
+    ...MarkdownComponents,
+  };
 }
