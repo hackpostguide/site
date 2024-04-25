1de12f52016b9fe5ff0a0344b4530f2140a8598b
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-25 00:18:30 -0400

    Refactor AboutPage component in routes/about/page.tsx

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { siteConfig } from "@/app/config/site";
 import { Link } from '@nextui-org/react';
 
@@ -7,18 +7,24 @@ const AboutPage = () => {
     <div>
       <h1>About Hackpost Guide 🚀</h1>
       <p>
-        Hackpost Guide is an open-source web app developed to consolidate resources for hackathons and foster a community passionate about creating apps, games, websites, and projects. It is designed after scratch.mit.edu and dev.to. 
+        Hackpost Guide is an open-source web app developed to consolidate resources for hackathons and to foster a community passionate about creating apps, games, websites, and projects. It is inspired by scratch.mit.edu and dev.to.
         <br /><br />
         Hackpost Guide promotes creativity and innovation, collaboration, and equity in computing.
         <br /><br />
-        For now, feel free to make an account and check out some posts/guides that others have created! Make sure to follow the community guidelines and maintain a positive and respectful environment. 
+        For now, feel free to create an account and explore posts and guides created by others! Please ensure you follow the community guidelines and maintain a positive and respectful environment.
         <br /><br />
       </p>
       <div className="text-lg leading-7">
-        This project is still VERY much a work in progress and in its early stages. If you find a bug, or if you would like to contribute, check out our <Link className="text-lg leading-7" isExternal href={siteConfig.links.github} aria-label="Discord">Github </Link>. Make sure to also join our <Link className="text-lg leading-7" isExternal href={siteConfig.links.discord} aria-label="Discord"> Discord Server </Link>.
+        This project is still very much a work in progress and in its early stages. If you encounter a bug, or if you would like to contribute, please check out our <Link className="text-lg leading-7" isExternal href={siteConfig.links.github} aria-label="Github">Github</Link>. Be sure to also join our <Link className="text-lg leading-7" isExternal href={siteConfig.links.discord} aria-label="Discord">Discord Server</Link>.
+      </div>
+      <div className='my-4'>
+        <h2 className='my-2'>Known Bugs</h2>
+        <p>Sometimes, the homepage and explore page do not render; instead, an <u>Application error: a client-side exception has occurred</u> appears, indicating a problem with hydration (Minified React error #423) along with a TypeError: Cannot destructure property &apos;parallelRouterKey&apos; of &apos;param&apos; as it is null. This bug is still present and being addressed. If you would like to assist, please visit our GitHub repository.</p>
+        <br />        
+        <p>If you discover more bugs or issues, let us know either in our Discord Server or on Github.</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default AboutPage
+export default AboutPage;
