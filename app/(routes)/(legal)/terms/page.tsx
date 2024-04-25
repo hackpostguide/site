b2153e5690ee4b71d6ecdcb65ff1c28f2f763869
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-25 09:46:51 -0400

    Set up markdown

--- app/(routes)/(legal)/terms/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TermsAndConditions = () => {
-  return (
-    <div>Terms And Conditions</div>
-  )
-}
-
-export default TermsAndConditions
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import createMDX from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -10,6 +12,12 @@ const nextConfig = {
         },
       ],
     },
+    // Configure `pageExtensions` to include markdown and MDX files
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   }
+
+  const withMDX = createMDX({
+    // Add markdown plugins here, as desired
+  })
   
-  export default nextConfig
+  export default withMDX(nextConfig)
