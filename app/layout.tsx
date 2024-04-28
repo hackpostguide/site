02672ddab989ef6101f341c32a342e9b18cdaa11
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-04-28 12:39:12 -0400

    Update layout and not-found components

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,13 +53,13 @@ export default function RootLayout({
       >
         <GoogleTagManager gtmId="G-WTDDS3XCDR" />
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark", children }}>
-          <div className="relative flex flex-col h-screen">
-            <Nav />
+          <Nav />
+          {/* <div className="relative flex flex-col h-screen"> */}
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
             <Footer />
-          </div>
+          {/* </div> */}
 		      <Toaster />
           <Analytics />
           <SpeedInsights />
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,8 @@ import { Link } from "@nextui-org/react";
  
 export default function NotFound() {
   return (
-    <div>
+    <div className="relative flex flex-col"> 
+    {/* h-screen */}
       <h3>404 - Page Not Found</h3>
       <p className="mb-4">Could not find requested resource</p>
       <Link href="/">
