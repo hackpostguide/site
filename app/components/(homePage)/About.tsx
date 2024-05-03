3f2ebb597f43aad3fb3e15fe6fba92b1a15c7fc5
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-05-03 17:48:56 -0400

    feat: Add TwitterIcon to Follow us on Social Media button

--- app/components/(homePage)/About.tsx
+++ app/components/(homePage)/About.tsx
@@ -2,15 +2,19 @@
 import { siteConfig } from '@/app/config/site'
 import { Button, Link } from '@nextui-org/react'
 import React from 'react'
-import { DiscordIcon, GithubIcon } from '../Icons'
+import { DiscordIcon, GithubIcon, TwitterIcon } from '../Icons'
 import { Icon } from '@iconify/react';
+import { title, subtitle } from "@/app/components/Primitives";
 
 const About = () => {
   return (
     <div className="p-8 flex flex-col md:items-start">
       <div className="mb-16 md:w-2/3">
-        <h2 className="mb-4">We are open source.</h2>
-        <p className="mb-4">
+        <h1 className={title()}>We are&nbsp;</h1>
+				<h1 className={title({color: "blue" })}>free</h1>
+        <h1 className={title()}> and </h1>
+        <h1 className={title({color: "blue" })}>open source.</h1>
+        <p className="my-4">
           The codebase is open source, and we welcome contributions from the community. If you find a bug, have a feature request, or want to contribute improvements, check out our GitHub repository.
         </p>
         <Button isExternal color="secondary" size='lg' as={Link} className='text-sm font-normal' variant='ghost' href={siteConfig.links.github}>
@@ -26,7 +30,10 @@ const About = () => {
             <DiscordIcon size={20} /> Join our Discord
           </Button>
           <Button isExternal color="success" size='lg' as={Link} className='text-sm font-normal' variant='ghost' href={siteConfig.links.linktree}>
-            <Icon icon="ph:linktree-logo-bold" width="1.4rem" height="1.4rem" /> Follow us on Social Media
+            <Icon icon="bi:instagram" width="1.4rem" height="1.4rem" /> 
+            <Icon icon="bi:youtube" width="1.4rem" height="1.4rem" /> 
+            <TwitterIcon />
+            Follow us on Social Media
           </Button>
         </div>
       </div>
