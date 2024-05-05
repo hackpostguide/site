67b28064d0f9b5b06864fecfe9b523176d5165e5
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-05-05 13:53:51 -0400

    refactor: Update About page to use larger font size for titles, improve scrolling animations, and add GitHub and Discord buttons

--- app/components/(homePage)/About.tsx
+++ app/components/(homePage)/About.tsx
@@ -53,7 +53,7 @@ const About = () => {
 
   return (
     <div className="p-8 flex flex-col md:items-start">
-      <div ref={leftRef} className={`mb-16 md:w-2/3 transition-all duration-1000 ${leftVisible ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-20'}`}>
+      <div ref={leftRef} className={`mb-16 md:w-3/4 transition-all duration-1000 ${leftVisible ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-20'}`}>
         <div>
           <h1 className={title({ size: "sm" })}>We are&nbsp;</h1>
           <h1 className={title({ size: "sm", color: "blue" })}>free</h1>
@@ -61,20 +61,29 @@ const About = () => {
           <h1 className={title({ size: "sm", color: "blue" })}>open source.</h1>
         </div>
         <p className="my-4">
-          The codebase is open source, and we welcome contributions from the community. If you find a bug, have a feature request, or want to contribute improvements, check out our GitHub repository.
+          The codebase is <u>open source</u>, and we welcome contributions from the community. If you find a bug, have a feature request, or want to contribute improvements, check out our GitHub repository. 
         </p>
-        <Button isExternal color="secondary" size='lg' as={Link} className='text-sm font-normal' variant='ghost' href={siteConfig.links.github}>
-          <GithubIcon size={20} /> Github
-        </Button>
-      </div>
-      <div ref={rightRef} className={`md:w-2/3 md:ml-auto flex flex-col items-start md:items-end md:text-right transition-all duration-1000 ${rightVisible ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-20'}`}>
         <p className="mb-4">
-          Likewise, make sure to also join our discord server! Our Discord server is a great place to get support, share knowledge, and be part of a vibrant community. We also encourage you to follow us on social media!
+          Likewise, make sure to also <u>join our discord server</u>! Our Discord server is a great place to get support, share knowledge, and be part of a vibrant community.
         </p>
-        <div className='flex flex-col md:flex-row gap-3'>
+        <div className='py-4 flex flex-col md:flex-row gap-3'>
+          <Button isExternal color="secondary" size='lg' as={Link} className='text-sm font-normal' variant='ghost' href={siteConfig.links.github}>
+            <GithubIcon size={20} /> Github
+          </Button>
           <Button isExternal color="primary" size='lg' as={Link} className='text-sm font-normal' variant='ghost' href={siteConfig.links.discord}>
             <DiscordIcon size={20} /> Join our Discord
           </Button>
+        </div>
+      </div>
+      <div ref={rightRef} className={`md:w-2/3 md:ml-auto flex flex-col items-start md:items-end md:text-right transition-all duration-1000 ${rightVisible ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-20'}`}>
+        <div>
+          <h1 className={title({ size: "sm" })}>Trusted by&nbsp;</h1>
+          <h1 className={title({ size: "sm", color: "violet" })}>Hundreds.</h1>
+        </div>
+        <p className="my-4">
+          Likewise, make sure to also join our discord server! Our Discord server is a great place to get support, share knowledge, and be part of a vibrant community. We also encourage you to follow us on social media!
+        </p>
+        <div className='py-4 flex flex-col md:flex-row gap-3'>
           <Button isExternal color="success" size='lg' as={Link} className='text-sm font-normal' variant='ghost' href={siteConfig.links.linktree}>
             <Icon icon="bi:instagram" width="1.4rem" height="1.4rem" />
             <Icon icon="bi:youtube" width="1.4rem" height="1.4rem" />
