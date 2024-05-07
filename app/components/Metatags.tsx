915fbc596658577d7fc76b934ac656c2c83b79e1
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-05-07 00:02:17 -0400

    refactor: Update Metatags component to use hardcoded values for title and description

--- app/components/Metatags.tsx
+++ app/components/Metatags.tsx
@@ -6,44 +6,46 @@ export default function Metatags({
         title = siteConfig.name,
         description = siteConfig.description,
         // image = 'https://www.hackpost.guide/apple-touch-icon.png',
-}) {
-        <NextSeo
-            title={title}
-            description={description}
-            canonical="https://www.hackpost.guide"
-            openGraph={{
-                url: 'https://www.hackpost.guide',
-                title: siteConfig.name,
-                description: siteConfig.description,
-                images: [
-                    {
-                        url: 'https://www.hackpost.guide/apple-touch-icon.png',
-                        width: 800,
-                        height: 800,
-                        alt: 'Og Image Alt',
-                        type: 'image/jpeg',
-                    },
-                ],
-                siteName:  siteConfig.name,
-            }}
-            twitter={{
-                handle: '@hackpostguide',
-                site: '@hackpostguide',
-                cardType: 'summary_large_image',
-            }}
-        />
-        // return (
-        //         <Head>
-        //                 <title>{title}</title>
-        //                 <meta name="twitter:card" content="summary" />
-        //                 {/* <meta name="twitter:site" content="@" /> */}
-        //                 <meta name="twitter:title" content={title} />
-        //                 <meta name="twitter:description" content={description} />
-        //                 {/* <meta name="twitter:image" content={image} /> */}
+}): JSX.Element {
+        return (
+            <>
+                <NextSeo
+                    title={'title'}
+                    description={'description'}
+                    canonical="https://www.hackpost.guide"
+                    openGraph={{
+                        url: 'https://www.hackpost.guide',
+                        title: 'title',
+                        description: 'description',
+                        images: [
+                            {
+                                url: 'https://www.hackpost.guide/apple-touch-icon.png',
+                                width: 800,
+                                height: 800,
+                                alt: 'Og Image Alt',
+                                type: 'image/jpeg',
+                            },
+                        ],
+                        siteName:  siteConfig.name,
+                    }}
+                    twitter={{
+                        handle: '@hackpostguide',
+                        site: '@hackpostguide',
+                        cardType: 'summary_large_image',
+                    }}
+                />
+            </>
+                // <Head>
+                //         <title>{title}</title>
+                //         <meta name="twitter:card" content="summary" />
+                //         {/* <meta name="twitter:site" content="@" /> */}
+                //         <meta name="twitter:title" content={title} />
+                //         <meta name="twitter:description" content={description} />
+                //         {/* <meta name="twitter:image" content={image} /> */}
 
-        //                 <meta property="og:title" content={title} />
-        //                 <meta property="og:description" content={description} />
-        //                 {/* <meta property="og:image" content={image} /> */}
-        //         </Head>
-        // );
+                //         <meta property="og:title" content={title} />
+                //         <meta property="og:description" content={description} />
+                //         {/* <meta property="og:image" content={image} /> */}
+                // </Head>
+        );
 }
