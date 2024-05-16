13cb658db572ccd9dd08c516597331f35c3f8197
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-05-16 16:26:00 -0400

    refactor: Improve UI and user experience in CreatePosts component

--- components/(homePage)/CreatePosts.tsx
+++ components/(homePage)/CreatePosts.tsx
@@ -1,25 +1,34 @@
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import { Card, CardContent } from '@/components/ui/card'
+'use client';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import React, { useContext } from 'react';
+import { Card, CardContent } from '@/components/ui/card';
+import { auth } from '@/lib/firebase';
+import { UserContext } from '@/lib/context';
 
 export const CreatePosts = () => {
-    return (
-        <div className=''>
-            <Card>
-                <CardContent className='mt-10 flex justify-center gap-10'>
-                    <div>
-                        <h2>Want to write guides?</h2>
-                        <p>It&apos;s free - Create an account today to start posting!</p> 
-                    </div>
-                    <Button color="warning" size='lg' className='text-sm font-bold my-8' asChild>
-                        <Link href="/enter">
-                            Create an Account
-                        </Link>
-                    </Button>
-                </CardContent>
-            </Card>
-            
-        </div>
-    )
-}
+  const { username } = useContext(UserContext);
+
+  return (
+    <div className=''>
+      <Card>
+        <CardContent className='mt-10 flex justify-center gap-10'>
+          <div>
+            <h2>Want to write guides?</h2>
+            <p>{username ? 'Go to your dashboard to start posting' : "It's free - Create an account today to start posting!"}</p>
+          </div>
+          <Button
+            color="warning"
+            size='lg'
+            className='text-sm font-bold my-8'
+            asChild
+          >
+            <Link href={username ? '/dashboard' : '/enter'}>
+              {username ? 'Go to Dashboard' : 'Create an Account'}
+            </Link>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
