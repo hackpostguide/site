0baf56f44fd3857f2ce853bdad3b596d8665be27
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-05-16 17:00:38 -0400

    chore: Add Google Tag Manager and Google Analytics scripts to layout.tsx and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import type { Viewport } from 'next';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script';
 import Footer from "../components/Footer";
 
 
@@ -43,14 +44,29 @@ export default function RootLayout({
 }>)  {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <head />
+      <head>
+        <Script async
+          id="google-tag-manager"
+          src="https://www.googletagmanager.com/gtag/js?id=G-GRQQQGTNL5">
+        </Script>
+        <Script
+           id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-GRQQQGTNL5');
+          `}
+        </Script>
+      </head>
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
-        <GoogleTagManager gtmId="G-WTDDS3XCDR" />
+        {/* <GoogleTagManager gtmId="G-WTDDS3XCDR" /> */}
         <Providers>
           <Nav />
           {/* <div className="relative flex flex-col h-screen"> */}
