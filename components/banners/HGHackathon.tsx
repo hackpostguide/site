855c5ef9d15b2327329a2da5df7860aa16af47bf
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-06-20 16:48:01 -0400

    refactor: Update Hackathon banner registration link

--- components/banners/HGHackathon.tsx
+++ components/banners/HGHackathon.tsx
@@ -13,7 +13,7 @@ const HGHackathonBanner = () => {
     <div className="bg-orange-300 text-black text-left sm:text-center font-bold p-3 relative">
       <span className="text-lg">The Hackpost Guide Hackathon is happening this summer! </span>
       <Link target="_blank" rel="noopener noreferrer" href={siteConfig.links.hackathon} className="text-lg ml-2 underline" title="Join Team">
-        <span className="text-default-600">Register now for fun and prizes </span>
+        <span className="text-default-600">Register now</span>
       </Link>
       <button 
         className="absolute right-4 top-1/2 transform -translate-y-1/2" 
