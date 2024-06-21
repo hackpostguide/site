0891d18d3a6f56939993d77fff6e6696caece964
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-06-20 23:03:05 -0400

    refactor: Add new partners to the Partners section

--- app/config/partners.ts
+++ app/config/partners.ts
@@ -3,10 +3,22 @@ export const partners = {
         {
             name: "SandCodes",
             image: "https://www.hackpost.guide/partners/Sandcodes.jpeg",
+            url: "https://sandcodes.com/",
         },
         {
             name: "IngeniumSTEM Summer Hacks 1.0",
             image: "https://www.hackpost.guide/partners/IngeniumStem-Technology.png",
+            url: "https://ingeniumstem.com/",
+        },
+        {
+            name: "Hackabyte 2024 Summer Hackathon",
+            image: "https://www.hackpost.guide/partners/Hackabyte-Logo.png",
+            url: "https://hackabyte.com/",
+        },
+        {
+            name: "Hackpost Guide Hackathon",
+            image: "https://www.hackpost.guide/partners/hg-hackathon.wbackground.png",
+            url: "https://www.hackpost.guide/",
         },
     ],
 };
