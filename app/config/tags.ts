e1fc1be0bfabf3a998fd7e40a80db4547f8d9f76
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-06-22 16:39:33 -0400

    refactor: Add tag manager component for managing post tags

---
+++ app/config/tags.ts
@@ -0,0 +1,94 @@
+export const tags = {
+    difficulty: [
+        {
+            name: "Beginner",
+            color: "bg-success",
+        },
+        {
+            name: "Intermediate",
+            color: "bg-warning",
+        },
+        {
+            name: "Advanced",
+            color: "bg-danger",
+        },
+    ],
+	topics: [
+        {
+            name: "React",
+            color: "bg-blue-600",
+        },
+        {
+            name: "Next.js",
+            color: "bg-black",
+        },
+        {
+            name: "Tailwind CSS",
+            color: "bg-cyan-600",
+        },
+        {
+            name: "TypeScript",
+            color: "bg-blue-500",
+        },
+        {
+            name: "JavaScript",
+            color: "bg-yellow-400",
+        },
+        {
+            name: "Python",
+            color: "bg-green-500",
+        },
+        {
+            name: "HTML",
+            color: "bg-red-600",
+        },
+        {
+            name: "CSS",
+            color: "bg-blue-400",
+        },
+        {
+            name: "Java",
+            color: "bg-yellow-600",
+        },
+        {
+            name: "Node.js",
+            color: "bg-green-800",
+        },
+        {
+            name: "Prisma",
+            color: "bg-pink-300",
+        },
+        {
+            name: "Firebase",
+            color: "bg-yellow-300",
+        },
+        {
+            name: "Netlify",
+            color: "bg-purple-500",
+        },
+        {
+            name: "Vercel",
+            color: "bg-black",
+        },
+        {
+            name: "GitHub",
+            color: "bg-gray-800",
+        },
+        {
+            name: "Git",
+            color: "bg-gray-700",
+        },
+        {
+            name: "LLMs",
+            color: "bg-blue-300",
+        },
+        {
+            name: "Tips",
+            color: "bg-red-300",
+        },
+        {
+            name: "Misc",
+            color: "bg-gray-400",
+        }
+    ],
+};
