a3ac49d3150fa728877416d439cd5ac38a62944f
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-06-23 14:39:02 -0400

    created Financial Support and Join Team components for homepage

---
+++ components/routeComponents/home/JoinTeam.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JoinTeam = () => {
+  return (
+    <div>JoinTeam</div>
+  )
+}
+
+export default JoinTeam
---
+++ components/routeComponents/home/SponsorDonate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SponsorDonate = () => {
+  return (
+    <div>Sponsor or Donate!</div>
+  )
+}
+
+export default SponsorDonate
