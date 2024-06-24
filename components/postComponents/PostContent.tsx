77d2766bca4ab35ef7134f27de58832d6900fb2f
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-06-23 22:55:13 -0400

    refactor: Add separator and display post description in PostContent component

--- components/postComponents/PostContent.tsx
+++ components/postComponents/PostContent.tsx
@@ -4,6 +4,7 @@ import { MarkdownComponents } from './MarkdownComponents';
 import { Card, CardHeader } from '@/components/ui/card';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import { Separator } from '../ui/separator';
 
 export default function PostContent({ post, path }: { post: any, path: string }) {
   const createdAt = typeof post?.createdAt === 'number' ? new Date(post.createdAt) : post.createdAt.toDate();
@@ -29,6 +30,8 @@ export default function PostContent({ post, path }: { post: any, path: string })
             </Button>
             <p className="">on {formattedDate}</p>
           </div>
+          <Separator className="my-4" />
+          <p className="text-foreground/85 font-bold text-center">{post.description}</p>
         </CardHeader>
       </Card>
       <ReactMarkdown components={MarkdownComponents} className="my-5">{post?.content}</ReactMarkdown>
