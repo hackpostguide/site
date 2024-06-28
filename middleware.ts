f9d75423b00d9b06825c09de5770143adf554cf6
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-06-28 10:36:54 -0400

    fix possible typo in url

--- middleware.ts
+++ middleware.ts
@@ -6,11 +6,15 @@ console.log('Middleware loaded')
 export async function middleware(request: NextRequest) {
   console.log('Request URL:', request.nextUrl.toString())
 
+  // Check if the path is '/expore' and redirect to '/explore'
+  if (request.nextUrl.pathname === '/expore') {
+    return NextResponse.redirect(new URL('/explore', request.url))
+  }
 
   return NextResponse.next()
 }
 
 // Specify which routes this middleware should be applied to
 export const config = {
-  matcher: ['/', '/about/:path*', '/dashboard/:path*', '/enter/:path*'],
+  matcher: ['/', '/about/:path*', '/dashboard/:path*', '/enter/:path*', '/expore', '/explore'],
 }
