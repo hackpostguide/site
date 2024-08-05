6a8f2564ee3906f331daf51b94485b00f8dd0bee
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-08-05 09:59:09 -0400

    updated card UI

--- components/routeComponents/home/Partners.tsx
+++ components/routeComponents/home/Partners.tsx
@@ -14,17 +14,15 @@ export const Partners = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
         {partners.hackathons.map((partner, index) => (
           <Link key={index} href={partner.url} target="_blank" className="block transform transition duration-300 hover:scale-105">
-            <Card className="bg-background text-foreground shadow-lg hover:shadow-xl transition-shadow duration-300 h-96 flex flex-col justify-between z-10 overflow-hidden">
-              <CardHeader className="flex-1 flex items-center justify-center p-6">
+            <Card className="bg-background text-foreground shadow-lg hover:shadow-xl transition-shadow duration-300 h-96 flex flex-col justify-center items-center z-10 overflow-hidden">
+              <CardContent className="flex flex-col items-center justify-center p-6 h-full w-full">
                 <Image 
                   src={partner.image} 
                   alt={partner.name} 
                   width={240} 
                   height={120} 
-                  className="object-contain max-h-48"
+                  className="object-contain max-h-48 mb-6"
                 />
-              </CardHeader>
-              <CardContent className="px-6 pb-6">
                 <CardTitle className="text-3xl font-bold text-center">{partner.name}</CardTitle>
               </CardContent>
             </Card>
